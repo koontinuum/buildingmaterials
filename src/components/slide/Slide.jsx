dd234fdb8a78511cb7cce85a183777f8d89fdbf7
Your Name <you@example.com>
2023-05-17 20:05:25 +0600

    feat/fix slider

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,14 +1,12 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import css from "./Slide.module.css";
-import click from "../../assets/different-images/click.svg";
 
-import React, { useRef } from "react";
+import React from "react";
 import Slider from "react-slick";
 import { slideContents } from "../../objects/product";
 
 function Slide() {
-  const arrowRef = useRef(null);
   var settings = {
     dots: true,
     infinite: true,
@@ -18,23 +16,23 @@ function Slide() {
     autoplay: true,
     autoplaySpeed: 3000,
     pauseOnHover: true,
-    arrows: false,
+    arrows: true,
 
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 770,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           initialSlide: 2,
         },
       },
@@ -50,26 +48,15 @@ function Slide() {
   return (
     <div className={css.slide__wrapper}>
       <h1>Материалы высшего качества</h1>
-      <button
-        className={css.nextClick}
-        onClick={() => arrowRef.current.slickNext()}
-      >
-        <img src={click} alt="" />
-      </button>
-      <Slider ref={arrowRef} {...settings}>
+      <Slider {...settings}>
         {slideContents.map((item) => (
           <div className={css.card}>
             <img src={item.img} alt="slide" />
             <p>{item.description}</p>
+            <a href="">Оформить заказ</a>
           </div>
         ))}
       </Slider>
-      <button
-        className={css.prevClick}
-        onClick={() => arrowRef.current.slickPrev()}
-      >
-        <img src={click} alt="" />
-      </button>
     </div>
   );
 }
