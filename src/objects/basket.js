2826c0ab9c638268bc78aa7e962331d7365d0b04
Your Name <you@example.com>
2023-05-21 17:52:32 +0600

    feat/add basket

---
+++ src/objects/basket.js
@@ -0,0 +1,18 @@
+import basket1 from '../assets/different-images/armatury.jpg'
+
+export const basket = [
+    {
+        id: 1,
+        img: basket1,
+        title: "Арматура",
+        descrip: "10 мм 5,85 м рифленая A500С",
+        price: "33,00"
+    },
+    {
+        id: 2,
+        img: basket1,
+        title: "Арматура",
+        descrip: "10 мм 5,85 м рифленая A500С",
+        price: "36,00"
+    }
+]
